8dfcf85458d0dc0026d1bb4bb7818b6c661bff76
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-15 05:19:16 -0300

    init

---
+++ lib/ignite_queue.ex
@@ -0,0 +1,18 @@
+defmodule IgniteQueue do
+  @moduledoc """
+  Documentation for `IgniteQueue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IgniteQueue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ignite_queue_test.exs
@@ -0,0 +1,8 @@
+defmodule IgniteQueueTest do
+  use ExUnit.Case
+  doctest IgniteQueue
+
+  test "greets the world" do
+    assert IgniteQueue.hello() == :world
+  end
+end
